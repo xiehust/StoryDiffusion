b1812b2eb272cc0cf6c35812f94b611f1e219a6e
xie river <chuanxie@amazon.com>
2024-05-10 13:23:41 +0000

    add sdxl basic server

---
+++ sdxl/api.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI, Request
+import uvicorn, json, datetime
+import torch
+from pydantic import BaseModel,Field
+from typing import Literal,List, Any
+from io import BytesIO
+import base64
+from PIL import Image
+from diffusers import DiffusionPipeline
+
+pipe = None
+DEVICE = "cuda"
+DEVICE_ID = "0"
+CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
+
+
+def torch_gc():
+    if torch.cuda.is_available():
+        with torch.cuda.device(CUDA_DEVICE):
+            torch.cuda.empty_cache()
+            torch.cuda.ipc_collect()
+
+def load_model():
+    global pipe
+    pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+    pipe.to("cuda")
+
+def base64_to_image(base64_string):
+    """
+    Converts a base64 string to a PIL.Image.Image object.
+    
+    Args:
+        base64_string (str): The base64 string representation of the image.
+        
+    Returns:
+        PIL.Image.Image: The image object.
+    """
+    image_bytes = base64.b64decode(base64_string)
+    image_buffer = BytesIO(image_bytes)
+    image = Image.open(image_buffer)
+    return image
+
+
+def image_to_base64(image):
+    """
+    Converts a PIL.Image.Image object to a base64 string.
+    
+    Args:
+        image (PIL.Image.Image): The image object to be converted.
+        
+    Returns:
+        str: The base64 string representation of the image.
+    """
+    buffer = BytesIO()
+    image.save(buffer, format="PNG")  # Use format="JPEG" for JPEG images
+    image_bytes = buffer.getvalue()
+    base64_string = base64.b64encode(image_bytes).decode("utf-8")
+    return base64_string
+
+
+class APIRequest(BaseModel):
+    num_steps : int = Field(default=50)
+    cfg_scale: float = Field(default=7)
+    negative_prompt : str =  Field(default="naked, deformed, bad anatomy, disfigured, poorly drawn face, mutation, extra limb, ugly, disgusting, poorly drawn hands, missing limb, floating limbs, disconnected limbs, blurry, watermarks, oversaturated, distorted hands, amputation")
+    prompt: str
+    
+class APIResponse(BaseModel):
+    images_base64 : List[str] = Field(..., descrition="Generated images")
+    
+app = FastAPI()
+
+@app.get("/ping")
+def ping():
+    return {'status': 'Healthy'}
+
+@app.post("/invocations")
+async def create_item(request: APIRequest):
+    global pipe
+    if pipe is None:
+        load_model()
+    print(request.prompt)
+    prompt=request.prompt
+    images = pipe(prompt=prompt).images
+
+    return APIResponse(images_base64=images)
+
+
+if __name__ == '__main__':
+    load_model()
+    uvicorn.run(app, host='0.0.0.0', port=8080, workers=1)
+    print('server start')
